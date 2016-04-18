c0a5d0bbb16e6fc47e318062c57fb9b7632d3a0e
selesdepselesnul <selesdepselesnul@gmail.com>
2016-04-18 15:54:47 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
 	$('#work').css('visibility', 'hidden');
 	$('#contact').css('visibility', 'hidden');
-	$('#horizontalHeader').css('display', 'none');
+	$('#horizontalHeader').css('visibility', 'hidden');
 	$(window).scroll(function(e) {
-		if ($(window).scrollTop() >= $('#work').offset().top) {
+		if ($(window).scrollTop() >= $('#work').offset().top 
+			&& $(window).scrollTop() <= $('#contact').offset().top) {
 			$('#work').css('visibility', '');
 			$(document.body).css('background-color', 'white');
+			$('#bigHeader').css('visibility', 'hidden');
+			$('#horizontalHeader').css('visibility', '');
+			console.log('work');
 		} else {
+			console.log('not work part');
+			$('#horizontalHeader').css('visibility', 'hidden');
+			$('#bigHeader').css('visibility', '');
 			$('#work').css('visibility', 'hidden');
 			$(document.body).css('background-color', 'black');
 		}
