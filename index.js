eedcebf15251c911ba3f9d8e4d596b5134840bac
selesdepselesnul <selesdepselesnul@gmail.com>
2016-04-18 19:59:13 +0700

    make it beautiful

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ $(document).ready(function() {
 	$('#work').css('visibility', 'hidden');
 	$('#contact').css('visibility', 'hidden');
 	$('#horizontalHeader').css('visibility', 'hidden');
+	var animated = false;
 	$(window).scroll(function(e) {
 		if ($(window).scrollTop() >= $('#work').offset().top 
 			&& $(window).scrollTop() <= $('#contact').offset().top) {
 			$('#horizontalHeader').css('visibility', '');
 			$('#work').css('visibility', '');
-			$('#work').animateCSS('fadeIn', {duration: 3000});
 			$(document.body).css('background-color', 'grey');
 			$('#bigHeader').css('visibility', 'hidden');
 			
-			console.log('work');
+			if(!animated) {
+				$('#work').animateCSS('zoomIn');
+				$('#horizontalHeader').animateCSS('slideInLeft');
+				animated = true;			
+			}
 		} else {
 			console.log('not work part');
 			$('#horizontalHeader').css('visibility', 'hidden');
 			$('#bigHeader').css('visibility', '');
 			$('#work').css('visibility', 'hidden');
 			$(document.body).css('background-color', 'black');
+			animated = false;
 		}
 	});
 
