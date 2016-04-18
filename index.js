228b6549147ee336e6a795e3eda5e7862d952b4b
selesdepselesnul <selesdepselesnul@gmail.com>
2016-04-18 16:10:02 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$('#work').css('visibility', 'hidden');
-	$('#contact').css('visibility', 'hidden');
+	// $('#contact').css('visibility', 'hidden');
 	$('#horizontalHeader').css('visibility', 'hidden');
 	$(window).scroll(function(e) {
 		if ($(window).scrollTop() >= $('#work').offset().top 
 			&& $(window).scrollTop() <= $('#contact').offset().top) {
 			$('#work').css('visibility', '');
-			$(document.body).css('background-color', 'white');
+			$(document.body).css('background-color', 'grey');
 			$('#bigHeader').css('visibility', 'hidden');
 			$('#horizontalHeader').css('visibility', '');
 			console.log('work');
