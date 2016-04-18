0691e18261373d610bad5be15270e4edb66cc053
selesdepselesnul <selesdepselesnul@gmail.com>
2016-04-18 19:43:04 +0700

    add animation

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
 $(document).ready(function() {
-	// $('#work').css('visibility', 'hidden');
-	// $('#contact').css('visibility', 'hidden');
-	// $('#horizontalHeader').css('visibility', 'hidden');
-	// $(window).scroll(function(e) {
-	// 	if ($(window).scrollTop() >= $('#work').offset().top 
-	// 		&& $(window).scrollTop() <= $('#contact').offset().top) {
-	// 		$('#work').css('visibility', '');
-	// 		$(document.body).css('background-color', 'grey');
-	// 		$('#bigHeader').css('visibility', 'hidden');
-	// 		$('#horizontalHeader').css('visibility', '');
-	// 		console.log('work');
-	// 	} else {
-	// 		console.log('not work part');
-	// 		$('#horizontalHeader').css('visibility', 'hidden');
-	// 		$('#bigHeader').css('visibility', '');
-	// 		$('#work').css('visibility', 'hidden');
-	// 		$(document.body).css('background-color', 'black');
-	// 	}
-	// });
+	$('#work').css('visibility', 'hidden');
+	$('#contact').css('visibility', 'hidden');
+	$('#horizontalHeader').css('visibility', 'hidden');
+	$(window).scroll(function(e) {
+		if ($(window).scrollTop() >= $('#work').offset().top 
+			&& $(window).scrollTop() <= $('#contact').offset().top) {
+			$('#horizontalHeader').css('visibility', '');
+			$('#work').css('visibility', '');
+			$('#work').animateCSS('fadeIn', {duration: 3000});
+			$(document.body).css('background-color', 'grey');
+			$('#bigHeader').css('visibility', 'hidden');
+			
+			console.log('work');
+		} else {
+			console.log('not work part');
+			$('#horizontalHeader').css('visibility', 'hidden');
+			$('#bigHeader').css('visibility', '');
+			$('#work').css('visibility', 'hidden');
+			$(document.body).css('background-color', 'black');
+		}
+	});
 
-	// $('#work').jScrollability(
-	// 	1,
-	// 	0,
-	// 	function($el,pcnt) {
-	// 		console.log(pcnt)
-	// 	 	$el.css('opacity', parseFloat($el.css('opacity'))-pcnt);
-	// });
-	// $('#work').jScrollability(
-	//     'self',
-	//     'self',
-	//     function($el,pcnt) {
-	//         // console.log()
-	//         // console.log($el);
-	//         console.log(pcnt)
-	//     }
-	// );
+
 });
