1d37d9312949d3a425e16b6c4d709f1613e94705
selesdepselesnul <selesdepselesnul@gmail.com>
2016-04-19 06:23:05 +0700

    update

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
 $(document).ready(function() {
 	$('#work').css('visibility', 'hidden');
-	// $('#contact').css('visibility', 'hidden');
+	$('#contact').css('visibility', 'hidden');
 	$('#verticalHeader').css('visibility', 'hidden');
-	var animated = false;
+	var animated = {
+		about : false,
+		work : false,
+		contact: false
+	};
 
 	$(window).scroll(function(e) {
-
-
 		if ($(window).scrollTop() >= $('#work').offset().top 
 			&& $(window).scrollTop() <= $('#work').outerHeight()) {
-			if(!animated) {
+			if(!animated.work) {
 				$('#verticalHeader').css('visibility', '');
 				$('#work').css('visibility', '');
 				$(document.body).css('background-color', 'grey');
 				$('#bigHeader').css('visibility', 'hidden');
 				$('#work').animateCSS('zoomIn');
 				$('#verticalHeader').animateCSS('slideInLeft');
-				animated = true;			
+				animated.work = true;			
 			}
+			animated.about = false;
+			animated.contact = false;
 		} else if($(window).scrollTop() > $('#work').outerHeight()){
 			console.log('not work part');
-			animated = false;
+			if(!animated.contact) {
+				$('#verticalHeader').css('visibility', '');
+				$('#work').css('visibility', '');
+				$(document.body).css('background-color', 'grey');
+				$('#bigHeader').css('visibility', 'hidden');
+				$('#work').animateCSS('zoomIn');
+				$('#verticalHeader').animateCSS('slideInLeft');
+				animated.contact = true;			
+			}
+			animated.work = false;
+			animated.contact = false
 		} else {
-			$('#verticalHeader').css('visibility', 'hidden');
-			$('#bigHeader').css('visibility', '');
-			$('#work').css('visibility', 'hidden');
-			$(document.body).css('background-color', 'black');
-			animated = false;
+			if(!animated.about) {
+				$('#verticalHeader').css('visibility', 'hidden');
+				$('#bigHeader').css('visibility', '');
+				$('#work').css('visibility', 'hidden');
+				$(document.body).css('background-color', 'black');
+				animated.about = true;	
+			}
+			animated.work = false;
+			animated.contact = false;
 		}
 	});
 });
