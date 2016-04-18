2ea4bde7bc80edd23424676bdb990db4c41d19b3
selesdepselesnul <selesdepselesnul@gmail.com>
2016-04-19 06:17:01 +0700

    refactor

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 $(document).ready(function() {
 	$('#work').css('visibility', 'hidden');
-	$('#contact').css('visibility', 'hidden');
-	$('#horizontalHeader').css('visibility', 'hidden');
+	// $('#contact').css('visibility', 'hidden');
+	$('#verticalHeader').css('visibility', 'hidden');
 	var animated = false;
+
 	$(window).scroll(function(e) {
+
+
 		if ($(window).scrollTop() >= $('#work').offset().top 
-			&& $(window).scrollTop() <= $('#contact').offset().top) {
-			$('#horizontalHeader').css('visibility', '');
-			$('#work').css('visibility', '');
-			$(document.body).css('background-color', 'grey');
-			$('#bigHeader').css('visibility', 'hidden');
-			
+			&& $(window).scrollTop() <= $('#work').outerHeight()) {
 			if(!animated) {
+				$('#verticalHeader').css('visibility', '');
+				$('#work').css('visibility', '');
+				$(document.body).css('background-color', 'grey');
+				$('#bigHeader').css('visibility', 'hidden');
 				$('#work').animateCSS('zoomIn');
-				$('#horizontalHeader').animateCSS('slideInLeft');
+				$('#verticalHeader').animateCSS('slideInLeft');
 				animated = true;			
 			}
-		} else {
+		} else if($(window).scrollTop() > $('#work').outerHeight()){
 			console.log('not work part');
-			$('#horizontalHeader').css('visibility', 'hidden');
+			animated = false;
+		} else {
+			$('#verticalHeader').css('visibility', 'hidden');
 			$('#bigHeader').css('visibility', '');
 			$('#work').css('visibility', 'hidden');
 			$(document.body).css('background-color', 'black');
 			animated = false;
 		}
 	});
-
-
 });
