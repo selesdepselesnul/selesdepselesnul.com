5a1e9eff7166fdf945f1560ab7646db61a4ad32a
selesdepselesnul <selesdepselesnul@gmail.com>
2016-04-26 13:21:02 +0700

    add disqus integration for comment

--- single.php
+++ single.php
@@ -5,15 +5,16 @@
 				the_post();
 				the_title('<h1>', '</h1><br />');
 				the_content();
-				echo "<br /><br />";
 			?>
 			<div class="text-left">
 				<?php previous_post_link();?>
 			</div>
 			<div class="text-right">
 				<?php next_post_link();?>
-			</div>		
+			</div>
+			<div id="disqus_thread"></div>		
 		</div>
+
 		<div class="col-md-2">
 			<div class="single-nav-menu">
 				<ul class="nav nav-pills nav-stacked">
@@ -36,5 +37,25 @@
 <script>
 	$(document.body).css('background-color', '#fff');
 	$('*').css('color', '#000');
+	 /**
+     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+     */
+    /*
+    var disqus_config = function () {
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+        var d = document, s = d.createElement('script');
+        
+        s.src = '//selesdepselesnul.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+        
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
 </script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+
 <?php get_footer(); ?>
